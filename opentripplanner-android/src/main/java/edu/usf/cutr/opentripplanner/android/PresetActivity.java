0c66b8c64faffcd405dbe4ef182b315e6385d9a3
francesca.grillo <francesca.grillo@accenture.com>
2019-12-08 15:29:51 +0100

    Create le activity Preset e Custom vuote che si possono raggiungere cliccando i respettivi tasti nella pagina iniziale

---
+++ opentripplanner-android/src/main/java/edu/usf/cutr/opentripplanner/android/PresetActivity.java
@@ -0,0 +1,13 @@
+package edu.usf.cutr.opentripplanner.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PresetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preset);
+    }
+}
--- opentripplanner-android/src/main/java/edu/usf/cutr/opentripplanner/android/RouteActivity.java
+++ opentripplanner-android/src/main/java/edu/usf/cutr/opentripplanner/android/RouteActivity.java
@@ -1,7 +1,9 @@
 package edu.usf.cutr.opentripplanner.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RouteActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class RouteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
+    }
+
+    public void preset(View view) {
+        Intent intent = new Intent(RouteActivity.this, PresetActivity.class);
+        startActivity(intent);
+    }
+
+    public void custom(View view) {
+        Intent intent = new Intent(RouteActivity.this, CustomActivity.class);
+        startActivity(intent);
     }
 }
