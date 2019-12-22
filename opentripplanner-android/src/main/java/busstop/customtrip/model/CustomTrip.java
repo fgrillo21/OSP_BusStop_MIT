806b550015bb05352ade3093fbfa1d808b8b5bbb
francesca.grillo <francesca.grillo@accenture.com>
2019-12-22 14:39:37 +0100

    Inizializzazione della classe customTrip

--- opentripplanner-android/src/main/java/busstop/customtrip/model/CustomTrip.java
+++ opentripplanner-android/src/main/java/busstop/customtrip/model/CustomTrip.java
@@ -1,5 +1,54 @@
 package busstop.customtrip.model;
 
 public class CustomTrip {
+    private final int monuments;
+    private final int greenAreas;
+    private final int openSpaces;
 
+    public static CustomTripBuilder newActivityGroup() {
+        return new CustomTripBuilder();
+    }
+
+    private CustomTrip(int monuments, int greenAreas, int openSpaces) {
+        this.monuments = monuments;
+        this.greenAreas = greenAreas;
+        this.openSpaces = openSpaces;
+    }
+
+    public int getMonuments() {
+        return monuments;
+    }
+
+    public int getGreenAreas() {
+        return greenAreas;
+    }
+
+    public int getOpenSpaces() {
+        return openSpaces;
+    }
+
+    public static final class CustomTripBuilder {
+        private int monuments;
+        private int greenAreas;
+        private int openSpaces;
+
+        public CustomTripBuilder withMonuments(int monuments) {
+            this.monuments = monuments;
+            return this;
+        }
+
+        public CustomTripBuilder withGreenAreas(int greenAreas) {
+            this.greenAreas = greenAreas;
+            return this;
+        }
+
+        public CustomTripBuilder withOpenSpaces(int openSpaces) {
+            this.openSpaces = openSpaces;
+            return this;
+        }
+
+        public CustomTrip build() {
+            return new CustomTrip(monuments, greenAreas, openSpaces);
+        }
+    }
 }
