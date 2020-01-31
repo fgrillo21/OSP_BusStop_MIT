b568e4bdbcdc29fe12b525cd98d4fdcfa03885c8
francesca.grillo <francesca.grillo@accenture.com>
2020-01-31 19:00:42 +0100

    - I nuovi parametri aggiunti nella classe CustomTrip sono stati valorizzati nel costruttore, nel builder - Sono stati creati i rispettivi getter

--- opentripplanner-android/src/main/java/busstop/customtrip/model/CustomTrip.java
+++ opentripplanner-android/src/main/java/busstop/customtrip/model/CustomTrip.java
@@ -8,9 +8,9 @@ public class CustomTrip implements Serializable {
     private final float monuments;
     private final float greenAreas;
     private final float openSpaces;
-    private int maxStops;
-    private int maxDurationMinutes;
-    private List<Place> intermediatePlaces;
+    private final int maxStops;
+    private final int maxDurationMinutes;
+    private final List<Place> intermediatePlaces;
 
     public static final int MAX = 100;
 
@@ -18,10 +18,13 @@ public class CustomTrip implements Serializable {
         return new CustomTripBuilder();
     }
 
-    private CustomTrip(float monuments, float greenAreas, float openSpaces) {
+    private CustomTrip(float monuments, float greenAreas, float openSpaces, int maxStops, int maxDurationMinutes, List<Place> intermediatePlaces) {
         this.monuments = monuments;
         this.greenAreas = greenAreas;
         this.openSpaces = openSpaces;
+        this.maxStops = maxStops;
+        this.maxDurationMinutes = maxDurationMinutes;
+        this.intermediatePlaces = intermediatePlaces;
     }
 
     public float getMonuments() {
@@ -36,10 +39,25 @@ public class CustomTrip implements Serializable {
         return openSpaces;
     }
 
+    public int getMaxStops() {
+        return maxStops;
+    }
+
+    public int getMaxDurationMinutes() {
+        return maxDurationMinutes;
+    }
+
+    public List<Place> getIntermediatePlaces() {
+        return intermediatePlaces;
+    }
+
     public static final class CustomTripBuilder {
         private float monuments;
         private float greenAreas;
         private float openSpaces;
+        private int maxStops;
+        private int maxDurationMinutes;
+        private List<Place> intermediatePlaces;
 
         public CustomTripBuilder withMonuments(float monuments) {
             this.monuments = monuments;
@@ -56,8 +74,23 @@ public class CustomTrip implements Serializable {
             return this;
         }
 
+        public CustomTripBuilder withMaxStops(int maxStops) {
+            this.maxStops = maxStops;
+            return this;
+        }
+
+        public CustomTripBuilder withMaxDurationMinutes(int maxDurationMinutes) {
+            this.maxDurationMinutes = maxDurationMinutes;
+            return this;
+        }
+
+        public CustomTripBuilder withIntermediatePlaces(List<Place> intermediatePlaces) {
+            this.intermediatePlaces = intermediatePlaces;
+            return this;
+        }
+
         public CustomTrip build() {
-            return new CustomTrip(monuments, greenAreas, openSpaces);
+            return new CustomTrip(monuments, greenAreas, openSpaces, maxStops, maxDurationMinutes, intermediatePlaces);
         }
     }
 
